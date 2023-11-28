56c950c964f45790c95fd1761919414c5f2211ef
NGALA MAC <macngala4@gmail.com>
2023-11-28 13:08:58 +0100

    Add styling for splash screen/authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :rememberable, :validatable
 
   has_many :categories, dependent: :destroy
   has_many :businesses, through: :categories

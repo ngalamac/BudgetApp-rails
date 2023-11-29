0ed8161dff1676ef7423677555ca493acbaa3fba
NGALA MAC <macngala4@gmail.com>
2023-11-29 17:06:30 +0100

    updated signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable
+         validates :email, presence: true
+         validates :password, presence: true
+         validates :name, presence: true
 
   has_many :categories, dependent: :destroy
   has_many :businesses, through: :categories
